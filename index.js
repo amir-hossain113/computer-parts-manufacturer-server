876ac6c7e436d94524f3f479aeba214d79532f2d
Amir Hossain <amirhossain.cse113@gmail.com>
2022-05-23 01:15:29 +0600

    database-added

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xqjsu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db("computerPartsDb").collection("products");
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
